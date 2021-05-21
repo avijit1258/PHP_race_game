e4324786df3d986a63ab5dbe754d60e7e17f9f13
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-05-21 04:57:54 -0600

    tested Car, Track working

--- src/Run.php
+++ src/Run.php
@@ -3,6 +3,16 @@ include('Race.php');
 
 
 // run a race and print the results
- $test = new Race;
- $results = $test->runRace();
- print_r($results->getRoundResults());
+$test = new Race;
+$results = $test->runRace();
+print_r($results->getRoundResults());
+
+// $testCar = new Car(0);
+// print_r($testCar->getSpeedOnCurve());
+// print_r($testCar->getSpeedOnStraight());
+// print_r($testCar);
+
+// $testTrack = new Track(2000);
+// print_r($testTrack->getTrack());
+
+
