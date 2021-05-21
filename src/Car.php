0db5de729d23ad17571dbd91de63711e07953835
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-05-21 00:56:46 -0600

    added car class with specification

--- src/Car.php
+++ src/Car.php
@@ -1,1 +1,30 @@
-Car.php
+<?php
+
+class Car
+{
+	private $id;
+    private $speed_on_curve;
+    private $speed_on_straight;
+    private $MINIMUM_SPEED = 4;
+    private $TOTAL_SPEED = 22;
+
+    // int $id
+
+    public function __construct(int $id)
+    {
+    	$this->id = $id;
+        $this->speed_on_curve = rand($this->MINIMUM_SPEED, $this->TOTAL_SPEED - $this->MINIMUM_SPEED);
+        $this->speed_on_straight = $this->TOTAL_SPEED - $this->speed_on_curve;
+    }
+
+    public function getSpeedOnCurve(): int
+    {
+    	return $this->speed_on_curve;
+    }
+
+    public function getSpeedOnStraight(): int
+    {
+    	return $this->speed_on_straight;
+    }
+
+}
