b86b268983f96350e914d35aead33496d69a68ee
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-05-24 17:03:21 -0600

    typo fixed in static syntax access

--- src/Car.php
+++ src/Car.php
@@ -6,15 +6,15 @@ class Car
 	private $id;
     private $speedOnCurve;
     private $speedOnStraight;
-    private $MINIMUM_SPEED = 4;
-    private $TOTAL_SPEED = 22;
+    private static $MINIMUM_SPEED = 4;
+    private static $TOTAL_SPEED = 22;
 
 
     public function __construct(int $id)
     {
     	$this->id = $id;
-        $this->speedOnCurve = rand($this->MINIMUM_SPEED, $this->TOTAL_SPEED - $this->MINIMUM_SPEED);
-        $this->speedOnStraight = $this->TOTAL_SPEED - $this->speedOnCurve;
+        $this->speedOnCurve = rand(self::$MINIMUM_SPEED, self::$TOTAL_SPEED - self::$MINIMUM_SPEED);
+        $this->speedOnStraight = self::$TOTAL_SPEED - $this->speedOnCurve;
 
     }
 
